6d06ee675366385c2fa33fba8e78f734aa038e5a
Dilpreet Maan <Dilpreet Maan@10.48.160.23>
2026-01-20 13:22:15 -0500

    Added some data members.

---
+++ src/github2/GItHub2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package github2;
+
+/**
+ *
+ * @author Dilpreet Maan
+ */
+public class GItHub2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/github2/LocalGit.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package github2;
+
+/**
+ *
+ * @author Dilpreet Maan
+ */
+public class LocalGit {
+    private int age;
+    
+}
